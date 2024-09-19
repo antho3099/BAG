f992c595575648c5609f44cf700f22bf6e48431c
ANG <antho.nguyen30@gmail.com>
2024-09-19 09:13:08 +0200

    Rendre le BAG meilleur

--- view/partial/footer.php
+++ view/partial/footer.php
@@ -1,5 +1,17 @@
 <footer>
-    <h3>La Baraque a Gaming</h3>
-    <a href="https://sparkinggeek.alwaysdata.net/ctrl/welcome/welcome.php"><img src="/img/logo/sparking_geek.webp" alt=""></a>
-    <p>Copyright@2024-Baraque_a_gaming</p>
+    <div id="socialfooter">
+        <img id="footerimg" src="/img/logo/BAG_legacy.webp" alt="">
+        <div id="social">
+            <h4>Retrouver la Baraque A Gaming sur:</h4>
+            <div id="socialmedia">
+                <ul>
+                    <li><a href="https://www.instagram.com/"><img src="/img/logo/instagram_label.webp" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="copyright">
+        <p>@2024CopyrightLaBaraque_A_Gaming</p>
+    </div>
+
 </footer>
